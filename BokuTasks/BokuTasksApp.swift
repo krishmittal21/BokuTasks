17b9d824014677262c063454764b5549de4b1d65
Krish Mittal <krishmittal212005@gmail.com>
2024-02-15 08:59:35 +0530

    Initial Commit

---
+++ BokuTasks/BokuTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BokuTasksApp.swift
+//  BokuTasks
+//
+//  Created by Krish Mittal on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BokuTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
