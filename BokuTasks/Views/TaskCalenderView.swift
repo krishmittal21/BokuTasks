3a19c7811c02a6de6683e2336f688975f981481c
Krish Mittal <krishmittal212005@gmail.com>
2024-02-15 09:56:31 +0530

    MVVM File System Setup

---
+++ BokuTasks/Views/TaskCalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCalenderView.swift
+//  BokuTasks
+//
+//  Created by Krish Mittal on 15/02/24.
+//
+
+import SwiftUI
+
+struct TaskCalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskCalenderView()
+}
