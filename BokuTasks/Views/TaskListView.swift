2de903338e898d8dc23caf37e6bccd158f7a5ad0
Krish Mittal <krishmittal212005@gmail.com>
2024-02-19 11:10:37 +0530

    Todo-Completed-Sections

--- BokuTasks/Views/TaskListView.swift
+++ BokuTasks/Views/TaskListView.swift
@@ -7,36 +7,62 @@
 
 import SwiftUI
 import FirebaseFirestoreSwift
+
 struct TaskListView: View {
+    
     @StateObject var viewModel: TaskListViewViewModel
     @FirestoreQuery var items: [TaskItem]
     private let userId: String
+    @State var isTodoSelected = true
+    
     init(userId: String){
         self.userId = userId
         self._items = FirestoreQuery(collectionPath: "users/\(userId)/tasks")
         self._viewModel = StateObject(wrappedValue: TaskListViewViewModel(userId: userId))
     }
+    
     var body: some View {
+        
         NavigationView{
             VStack{
-                List(items) { item in
-                    TaskListItemView(item: item)
-                        .swipeActions{
-                            Button("Delete"){
-                                viewModel.delete(id: item.id)
-                            }
-                            .tint(.red)
-                        }
+                HStack(spacing: 100){
+                    Button(action: {
+                        isTodoSelected = true
+                    }) {
+                        Text("Todo")
+                            .foregroundColor(isTodoSelected ? .blue : .black)
+                    }
+                    Button(action: {
+                        isTodoSelected = false
+                    }) {
+                        Text("Completed")
+                            .foregroundColor(isTodoSelected ? .black : .blue)
+                    }
                 }
-                .listStyle(PlainListStyle())
+                
+                
+                if isTodoSelected {
+                    tasksLeft
+                } else {
+                    tasksCompleted
+                }
+                
             }
             .navigationTitle("Tasks")
             .toolbar{
                 Button{
                     viewModel.showingNewItem = true
                 } label: {
-                    Image(systemName: "plus")
+                    ZStack{
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 1)
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                    }
+                    
                 }
+                
             }
             .sheet(isPresented: $viewModel.showingNewItem ){
                 NewItemView(newItemPresented: $viewModel.showingNewItem)
@@ -44,7 +70,40 @@ struct TaskListView: View {
         }
         
     }
+    
+    
+    @ViewBuilder
+    var tasksLeft: some View {
+        let filteredItems = items.filter { !$0.isDone }
+        List(filteredItems) { item in
+            TaskListItemView(item: item)
+                .swipeActions{
+                    Button("Delete"){
+                        viewModel.delete(id: item.id)
+                    }
+                    .tint(.red)
+                }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    
+    @ViewBuilder
+    var tasksCompleted: some View {
+        let filteredItems = items.filter { $0.isDone }
+        List(filteredItems) { item in
+            TaskListItemView(item: item)
+                .swipeActions{
+                    Button("Delete"){
+                        viewModel.delete(id: item.id)
+                    }
+                    .tint(.red)
+                }
+        }
+        .listStyle(PlainListStyle())
+    }
 }
+ 
 
 #Preview {
     TaskListView(userId: "HCbFqqujIRZXoDFGBGvhdIpVDpM2")
